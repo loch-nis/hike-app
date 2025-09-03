2931a337c81529665115ae082329a4b5b6cba835
Nis Studie <bruger@MacBook-Pro-tilhrende-Bruger.local>
2025-09-03 15:34:06 +0200

    test

--- hike-app-backend/app/Http/Services/HikeService.php
+++ hike-app-backend/app/Http/Services/HikeService.php
@@ -7,5 +7,5 @@ class HikeService
     // todo consider refactoring controller into this:
     // https://medium.com/@laravelprotips/understanding-laravel-service-classes-a-comprehensive-guide-1f22310c70bd
 
-    // or I could use actions instead of service classes, but honestly I might just leave the controller with all the logic for now to avoid creating an unnecessary abstraction. Reconsider if controller logic grows
+    // or I could use actions instead of service classes, but honestly I might just leave the controller with all the logic for now to avoid creating an unnecessary abstraction. Reconsider if controller logic grows.
 }
